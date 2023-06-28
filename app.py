e37ef6e975f36b0913e1cabb841ff6880db477da
Yussuf Aboobaker <118903589+theStacklessDev@users.noreply.github.com>
2023-06-28 19:31:05 +0200

    Update app.py
    Adding the import of the modules needed to make the code run

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import requests
+import tkinter
+
 window = tkinter.Tk()
-window.title("Password storage")
+window.title("Weather App")
 window.geometry("400x200")
 window.configure(bg="#0276AB")
 
@@ -7,9 +10,8 @@ frame = tkinter.Frame(bg="#0276AB")
 
 def weather():
     city_name = website_entry.get()
-    API_KEY = "insert API key here"
+    API_KEY = "dccf47c42397245dae4c99e1ba8be90e"
     BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
-
 
     request_url = f"{BASE_URL}?appid={API_KEY}&q={city_name}"
     response = requests.get(request_url)
@@ -19,31 +21,28 @@ def weather():
         weather = data['weather'][0]["description"]
         temperature = round(data["main"]["temp"] - 273, 2)
 
-        sky_condition_label = tkinter.Label(frame, text=weather , bg="#0276AB")
+        sky_condition_label = tkinter.Label(frame, text=weather, bg="#0276AB")
         sky_condition_label.grid(row=8, column=1)
 
-        weather_label = tkinter.Label(frame, text=str(temperature) + str(" Degress celcius"), bg="#0276AB")
+        weather_label = tkinter.Label(frame, text=str(temperature) + str(" Degrees Celsius"), bg="#0276AB")
         weather_label.grid(row=9, column=1)
 
-    elif city_name =="":
+    elif city_name == "":
         print("You haven't typed anything - type a city name")
 
     else:
         print("That's either not a city or a city I have information on at this moment. Try a different city")
 
 
-
-app_title = tkinter.Label(frame, text="Yussuf's secure password storage tool", bg="#0276AB" , font=("Calibri", 10))
+app_title = tkinter.Label(frame, text="Yussuf's secure password storage tool", bg="#0276AB", font=("Calibri", 10))
 website_label = tkinter.Label(frame, text="Weather city request? ", bg="#0276AB")
 website_entry = tkinter.Entry(frame)
 generate_button = tkinter.Button(frame, text="Generate weather forecast", command=weather)
 
-
-app_title.grid(row=2, column=1, pady= 20)
+app_title.grid(row=2, column=1, pady=20)
 website_entry.grid(row=3, column=1, padx=10, pady=10)
 website_label.grid(row=3, column=0)
-generate_button.grid(row=7,column=1)
-
+generate_button.grid(row=7, column=1)
 
 frame.pack()
 
